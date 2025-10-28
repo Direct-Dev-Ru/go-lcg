e7c11879a170590be88a58fa9a23c268fcf5e5e8
Anton Kuznetcov <info@direct-dev.ru>
2025-10-28 12:56:16 +0600

    Исправления в ветке main

--- serve/auth.go
+++ serve/auth.go
@@ -94,7 +94,7 @@ func validateJWTToken(tokenString string) (*JWTClaims, error) {
 	}
 
 	// Парсим токен
-	token, err := jwt.ParseWithClaims(tokenString, &JWTClaims{}, func(token *jwt.Token) (interface{}, error) {
+	token, err := jwt.ParseWithClaims(tokenString, &JWTClaims{}, func(token *jwt.Token) (any, error) {
 		// Проверяем метод подписи
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
 			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
